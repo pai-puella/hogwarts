39aa692c937fcbbc825e2c82cb7551e0b8bfdb88
pai-puella <cat.dyshkant@yandex.ru>
2023-11-14 08:37:56 -0800

    initial commit

---
+++ src/Hogwarts.java
@@ -0,0 +1,18 @@
+
+public class Hogwarts {
+    private int magicPower;
+    private int transgressionDistance;
+
+    public Hogwarts (int magicPower, int transgressionDistance){
+        this.magicPower = magicPower;
+        this.transgressionDistance = transgressionDistance;
+    }
+
+    public int getMagicPower() {
+        return magicPower;
+    }
+
+    public int getTransgressionDistance() {
+        return transgressionDistance;
+    }
+}
