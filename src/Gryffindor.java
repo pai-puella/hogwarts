94d634f12bf4a4d1c91ad40b29cd8bb0cc86a186
pai-puella <cat.dyshkant@yandex.ru>
2023-11-14 09:32:38 -0800

    Added describe and compare methods

--- src/Gryffindor.java
+++ src/Gryffindor.java
@@ -10,4 +10,26 @@ public class Gryffindor extends Hogwarts {
         this.honor = honor;
         this.bravery = bravery;
     }
+
+    public void describeStudent() {
+        System.out.println("Студент Гриффиндора: ");
+        System.out.println("Сила магии " + getMagicPower());
+        System.out.println("Расстояние трансгрессии " + getTransgressionDistance());
+        System.out.println("Благородство: " + nobility);
+        System.out.println("Честь: " + honor);
+        System.out.println("Храбрость: " + bravery);
+    }
+
+    public void compareStudents(Gryffindor student1, Gryffindor student2) {
+        int sum1 = student1.nobility + student1.honor + student1.bravery;
+        int sum2 = student2.nobility + student2.honor + student2.bravery;
+
+        if (sum1 > sum2) {
+            System.out.println("Первый студент лучше второго");
+        } else if (sum1 < sum2) {
+            System.out.println("Второй студент лучше первого");
+        } else {
+            System.out.println("Оба студента равны");
+        }
+    }
 }
