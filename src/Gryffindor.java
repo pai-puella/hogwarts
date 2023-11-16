c703911e2259c259d48d32f6e5d5874927bee945
pai-puella <cat.dyshkant@yandex.ru>
2023-11-16 12:40:53 -0800

    Edited describeStudent

--- src/Gryffindor.java
+++ src/Gryffindor.java
@@ -1,35 +1,42 @@
 public class Gryffindor extends Hogwarts {
+    private String studentName;
+    //private int studentQuality;
     private int nobility;
     private int honor;
     private int bravery;
 
-    public Gryffindor(int magicPower, int transgressionDistance,
-                      int nobility, int honor, int bravery) {
+    public Gryffindor(String studentName, int nobility, int honor, int bravery,
+                      int magicPower, int transgressionDistance) {
         super(magicPower, transgressionDistance);
+        this.studentName = studentName;
         this.nobility = nobility;
         this.honor = honor;
         this.bravery = bravery;
     }
 
     public void describeGrStudent() {
-        System.out.println("Студент Гриффиндора: ");
+        System.out.println("Студент Гриффиндора: " + studentName);
         System.out.println("Сила магии " + getMagicPower());
         System.out.println("Расстояние трансгрессии " + getTransgressionDistance());
+        System.out.println("Дополнительные качества факультета Гриффиндор: ");
         System.out.println("Благородство: " + nobility);
         System.out.println("Честь: " + honor);
         System.out.println("Храбрость: " + bravery);
     }
 
-    public void compareGrStudents(Gryffindor student1, Gryffindor student2) {
-        int sum1 = student1.nobility + student1.honor + student1.bravery;
-        int sum2 = student2.nobility + student2.honor + student2.bravery;
+    public String getStudentName() {
+        return studentName;
+    }
 
-        if (sum1 > sum2) {
-            System.out.println(student1 + " лучший Гриффиндорец, чем " + student2);
-        } else if (sum1 < sum2) {
-            System.out.println(student2 + " лучший Гриффиндорец, чем " + student1);
-        } else {
-            System.out.println("Оба студента равны");
-        }
+    public int getNobility() {
+        return nobility;
+    }
+
+    public int getBravery() {
+        return bravery;
+    }
+
+    public int getHonor() {
+        return honor;
     }
 }
